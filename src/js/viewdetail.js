4b935389ca729a375d7a89dfef8f943ad1a00321
Gonyo <sksgml1254@namsan-n.gwe.es.kr>
2023-06-06 01:03:16 +0900

    Initial commit

---
+++ src/js/viewdetail.js
@@ -0,0 +1,7 @@
+const playstatusBar = document.querySelector(".playstatus_bar")
+
+function onPlaystatusClick() {
+    location.href = "[✅] viewdetail.html"
+}
+
+playstatusBar.addEventListener("click", onPlaystatusClick);
